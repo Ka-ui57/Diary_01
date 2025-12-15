647e8d4dd147492f609ddd6435121fc35f4da431
yer <tn_sora@icloud.com>
2025-12-15 14:21:10 +0900

    Initial Commit

---
+++ Diary/DiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiaryApp.swift
+//  Diary
+//
+//  Created by からすす on 2025/12/15.
+//
+
+import SwiftUI
+
+@main
+struct DiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DiaryTests/DiaryTests.swift
@@ -0,0 +1,17 @@
+//
+//  DiaryTests.swift
+//  DiaryTests
+//
+//  Created by からすす on 2025/12/15.
+//
+
+import Testing
+@testable import Diary
+
+struct DiaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
